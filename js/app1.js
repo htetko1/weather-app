8df4b1c7582e7865febbacb759080300292c4f34
htetko <ko5602538@gmail.com>
2022-02-23 21:43:22 +0630

    test

---
+++ js/app1.js
@@ -0,0 +1,288 @@
+(function(e) {
+    function t(t) {
+        for (
+            var a, c, i = t[0], s = t[1], u = t[2], p = 0, d = []; p < i.length; p++
+        )
+            (c = i[p]),
+            Object.prototype.hasOwnProperty.call(n, c) && n[c] && d.push(n[c][0]),
+            (n[c] = 0);
+        for (a in s) Object.prototype.hasOwnProperty.call(s, a) && (e[a] = s[a]);
+        l && l(t);
+        while (d.length) d.shift()();
+        return o.push.apply(o, u || []), r();
+    }
+
+    function r() {
+        for (var e, t = 0; t < o.length; t++) {
+            for (var r = o[t], a = !0, i = 1; i < r.length; i++) {
+                var s = r[i];
+                0 !== n[s] && (a = !1);
+            }
+            a && (o.splice(t--, 1), (e = c((c.s = r[0]))));
+        }
+        return e;
+    }
+    var a = {},
+        n = {
+            app: 0,
+        },
+        o = [];
+
+    function c(t) {
+        if (a[t]) return a[t].exports;
+        var r = (a[t] = {
+            i: t,
+            l: !1,
+            exports: {},
+        });
+        return e[t].call(r.exports, r, r.exports, c), (r.l = !0), r.exports;
+    }
+    (c.m = e),
+    (c.c = a),
+    (c.d = function(e, t, r) {
+        c.o(e, t) ||
+            Object.defineProperty(e, t, {
+                enumerable: !0,
+                get: r,
+            });
+    }),
+    (c.r = function(e) {
+        "undefined" !== typeof Symbol &&
+            Symbol.toStringTag &&
+            Object.defineProperty(e, Symbol.toStringTag, {
+                value: "Module",
+            }),
+            Object.defineProperty(e, "__esModule", {
+                value: !0,
+            });
+    }),
+    (c.t = function(e, t) {
+        if ((1 & t && (e = c(e)), 8 & t)) return e;
+        if (4 & t && "object" === typeof e && e && e.__esModule) return e;
+        var r = Object.create(null);
+        if (
+            (c.r(r),
+                Object.defineProperty(r, "default", {
+                    enumerable: !0,
+                    value: e,
+                }),
+                2 & t && "string" != typeof e)
+        )
+            for (var a in e)
+                c.d(
+                    r,
+                    a,
+                    function(t) {
+                        return e[t];
+                    }.bind(null, a),
+                );
+        return r;
+    }),
+    (c.n = function(e) {
+        var t =
+            e && e.__esModule ?
+
+            function() {
+                return e["default"];
+            } :
+            function() {
+                return e;
+            };
+        return c.d(t, "a", t), t;
+    }),
+    (c.o = function(e, t) {
+        return Object.prototype.hasOwnProperty.call(e, t);
+    }),
+    (c.p = "/");
+    var i = (window["webpackJsonp"] = window["webpackJsonp"] || []),
+        s = i.push.bind(i);
+    (i.push = t), (i = i.slice());
+    for (var u = 0; u < i.length; u++) t(i[u]);
+    var l = s;
+    o.push([0, "chunk-vendors"]), r();
+})({
+    0: function(e, t, r) {
+        e.exports = r("56d7");
+    },
+    "034f": function(e, t, r) {
+        "use strict";
+        r("85ec");
+    },
+    "56d7": function(e, t, r) {
+        "use strict";
+        r.r(t);
+        r("e260"), r("e6cf"), r("cca6"), r("a79d");
+        var a = r("2b0e"),
+            n = function() {
+                var e = this,
+                    t = e.$createElement,
+                    r = e._self._c || t;
+                return r(
+                    "div", {
+                        class: [
+                            "undefined" != typeof e.weather.main && e.weather.main.temp > 16 ?
+                            "warm" :
+                            "",
+                        ],
+                        attrs: {
+                            id: "app",
+                        },
+                    }, [
+                        r("main", [
+                            r(
+                                "div", {
+                                    staticClass: "search-box",
+                                }, [
+                                    r("input", {
+                                        directives: [{
+                                            name: "model",
+                                            rawName: "v-model",
+                                            value: e.query,
+                                            expression: "query",
+                                        }, ],
+                                        staticClass: "search-bar",
+                                        attrs: {
+                                            type: "text",
+                                            placeholder: "search....",
+                                        },
+                                        domProps: {
+                                            value: e.query,
+                                        },
+                                        on: {
+                                            keypress: e.fetchWeather,
+                                            input: function(t) {
+                                                t.target.composing || (e.query = t.target.value);
+                                            },
+                                        },
+                                    }),
+                                ],
+                            ),
+                            "undefined" != typeof e.weather.main ?
+                            r(
+                                "div", {
+                                    staticClass: "weather-wrap",
+                                }, [
+                                    r(
+                                        "div", {
+                                            staticClass: "location-box",
+                                        }, [
+                                            r(
+                                                "div", {
+                                                    staticClass: "location",
+                                                }, [
+                                                    e._v(
+                                                        " " +
+                                                        e._s(e.weather.name) +
+                                                        "," +
+                                                        e._s(e.weather.sys.country) +
+                                                        " ",
+                                                    ),
+                                                ],
+                                            ),
+                                            r(
+                                                "div", {
+                                                    staticClass: "date",
+                                                }, [e._v(" " + e._s(e.dateBuilder()) + " ")],
+                                            ),
+                                        ],
+                                    ),
+                                    r(
+                                        "div", {
+                                            staticClass: "weather-box",
+                                        }, [
+                                            r(
+                                                "div", {
+                                                    staticClass: "temp",
+                                                }, [e._v(" " + e._s(e.weather.main.temp) + " °c ")],
+                                            ),
+                                            r(
+                                                "div", {
+                                                    staticClass: "weather",
+                                                }, [e._v(" " + e._s(e.weather.weather[0].main) + " ")],
+                                            ),
+                                        ],
+                                    ),
+                                ],
+                            ) :
+                            e._e(),
+                        ]),
+                    ],
+                );
+            },
+            o = [],
+            c =
+            (r("d3b7"),
+                r("99af"), {
+                    name: "App",
+                    data: function() {
+                        return {
+                            api_key: "f6ceb2d2c3672e5fa259822657f3accc",
+                            url_base: "https://api.openweathermap.org/data/2.5/",
+                            query: "",
+                            weather: {},
+                        };
+                    },
+                    methods: {
+                        fetchWeather: function(e) {
+                            "Enter" == e.key &&
+                                fetch(
+                                    ""
+                                    .concat(this.url_base, "weather?q=")
+                                    .concat(this.query, "&units=metric&APPID=")
+                                    .concat(this.api_key),
+                                )
+                                .then(function(e) {
+                                    return e.json();
+                                })
+                                .then(this.setResults);
+                        },
+                        setResults: function(e) {
+                            this.weather = e;
+                        },
+                        dateBuilder: function() {
+                            var e = new Date(),
+                                t = [
+                                    "January",
+                                    "February",
+                                    "March",
+                                    "April",
+                                    "May",
+                                    "June",
+                                    "July",
+                                    "August",
+                                    "September",
+                                    "October",
+                                    "November",
+                                    "December",
+                                ],
+                                r = [
+                                    "Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thursday",
+                                    "Friday",
+                                    "Saturday",
+                                    "Sunday",
+                                ],
+                                a = r[e.getDay()],
+                                n = e.getDay(),
+                                o = t[e.getMonth()],
+                                c = e.getFullYear();
+                            return "".concat(a, " ").concat(n, " ").concat(o, " ").concat(c);
+                        },
+                    },
+                }),
+            i = c,
+            s = (r("034f"), r("2877")),
+            u = Object(s["a"])(i, n, o, !1, null, null, null),
+            l = u.exports;
+        (a["a"].config.productionTip = !1),
+        new a["a"]({
+            render: function(e) {
+                return e(l);
+            },
+        }).$mount("#app");
+    },
+    "85ec": function(e, t, r) {},
+});
+//# sourceMappingURL=app.2062d738.js.map
